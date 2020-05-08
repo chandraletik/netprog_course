d6cb11bba9eda2ab1f31a1b3ddd9756bf78bc510
chandraletik <andraletiknew@gmail.com>
2020-05-08 17:27:04 +0800

    test commit message

--- mqtt/publisher.py
+++ mqtt/publisher.py
@@ -16,25 +16,25 @@ Connected = False   #global variable for the state of the connection
 
 # After you successfully the publisher and subscribe code, 
 # try to create your own MQTT broker by create an account at cloudmqtt.com
-broker_address= 'your cloud broker url'
-port = 'your cloud broker port' # do not type string, but change to integer
+broker_address= 'mqtt.eclipse.org'
+port = 1883 # do not type string, but change to integer
 username = 'your username'
 password = 'your password'
  
-client = mqttClient.Client('Python')               #create new instance
-client.username_pw_set(user, password=password)    #set username and password
+client = mqttClient.Client()               #create new instance
+#client.username_pw_set(user, password=password)    #set username and password
 client.on_connect= on_connect                      #attach function to callback
 client.connect(broker_address, port=port)          #connect to broker
  
 client.loop_start()        #start the loop
  
 while Connected != True:    #Wait for connection
-    time.sleep(0.1)
+    time.sleep(2)
  
 try:
     while True: 
-        value = input('Enter the message:')
-        client.publish('python/', value)
+        value = input('enter new massage:')
+        client.publish('python', value)
  
 except KeyboardInterrupt:
     client.disconnect()
